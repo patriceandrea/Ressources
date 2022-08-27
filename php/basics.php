ac73e0e9b3db25ac8c46cbf7c63b554e09c50d75
patriceandrea <andrea_pavico98@yahoo.ca>
2022-08-27 16:45:22 -0400

    adds datatypes in php

---
+++ php/basics.php
@@ -0,0 +1,44 @@
+
+
+<?php
+// Variables 
+
+// A variable is declared using a $ sign followed by variable name 
+// A variable name contain alphabets, numbers, underscore_
+// Variables  are case sensitive
+
+
+$string = "Patrice ";
+$int = 9;
+$float = 3.14;
+echo "My favorite number is $int \n";
+echo ("I am enclosed in parenthesis");
+
+?>
+
+
+<?php
+// datatypes 
+
+
+//bolean 
+$a = true;
+$b = false;
+
+//integer 
+$x = -7484;
+$y = 7007;
+
+
+//float
+$a = -2874.01;
+$b = 7007.70;
+
+
+//string 
+$str1 = "Hello";
+$str2 = "what is your favorite color?";
+
+
+echo $str1;
+?>
