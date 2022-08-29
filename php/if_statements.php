f896bd9551a577186f49c62227ccedf291e2c2c5
patriceandrea <andrea_pavico98@yahoo.ca>
2022-08-29 10:20:53 -0400

    finishes if statement

--- php/if_statements.php
+++ php/if_statements.php
@@ -1,6 +1,28 @@
 <?php
-$age = 20;
+$age = 26;
 
+//if statement 
 if ($age <= 25) {
   echo "You are not allowed to consume";
+}
+
+
+//if...else 
+if ($age <= 25) {
+  echo "You are not allowed to consume alcohol";
+} else {
+  echo "Enjoy the drinks";
+}
+
+
+//else if 
+
+$speed = 110;
+
+if ($speed < 60) {
+  echo "Safe driving speed";
+} elseif ($speed > 60 && $speed < 100) {
+  echo "You are burning extra fuel";
+} else {
+  echo " It's dangerous";
 }
