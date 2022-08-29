90cf9805ef20e9482c258b1374a3810b9aaf0a49
patriceandrea <andrea_pavico98@yahoo.ca>
2022-08-29 15:30:04 -0400

    adds array

---
+++ php/array.php
@@ -0,0 +1,91 @@
+<?php
+// // array(); 
+// // 3 types of array : 1- indexed array, 2- associative array, 3-multidimensional array
+
+
+// $lamborghinis = array("Urus", "Hurucan", "Aventador");
+
+// // //print the first car from the array
+// echo $lamborghinis[0];
+
+
+
+?> 
+
+
+<?php
+// indexed array 
+
+// $suzuki[0] = "Swift";
+// $suzuki[1] = "Baleno";
+// $suzuki[2] = "Ertiga";
+// $suzuki[3] = "Brezza";
+// $suzuki[4] = "Vitra";
+
+// echo $suzuki[0], "\n";
+// echo $suzuki[3], "\n";
+// echo $suzuki[2], "\n";
+
+// using for loop 
+
+// $size = count($lamborghinis);
+
+// for ($i = 0; $i < $size; $i++) {
+//   echo $lamborghinis[$i], "\n";
+// }
+
+// //for each 
+
+// foreach ($lamborghinis as $lambo) {
+//   echo $lambo . "\n";
+// }
+
+?>
+
+<?php
+//associative array 
+
+// $lamborghinis_ass = array("suv" => "Urus", "sports" => "Huracan", "coupe" => "Aventador");
+
+// echo $lamborghinis_ass["suv"], "\n";
+// echo $lamborghinis_ass["sports"], "\n";
+// echo $lamborghinis_ass["coupe"], "\n";
+
+// $suzuki["hatch"] = "Swift";
+// $suzuki["utility"] = "Ertiga";
+// $suzuki["suv"] = "Vitara";
+
+
+// echo $suzuki["hatch"], "\n";
+// echo $suzuki["utility"], "\n";
+// echo $suzuki["suv"], "\n";
+
+
+// // using for loop 
+
+// $size = count($lamborghinis_ass);
+// $keys = array_keys($lamborghinis_ass);
+
+
+// //$lamborghinis_ass[$keys[$i]] === value 
+// // $keys[$i] === array/object keys 
+
+// for ($i = 0; $i < $size; $i++) {
+//   echo $lamborghinis_ass[$keys[$i]] . "is a/an " . $keys[$i] . " car \n";
+// }
+
+// // using foreach loop 
+
+
+// foreach ($lamborghinis_ass as $key => $value) {
+//   echo $value . "is a/an " . $key . " car \n";
+// }
+
+?>
+
+<?php
+//multidimensional array 
+
+
+
+?>
