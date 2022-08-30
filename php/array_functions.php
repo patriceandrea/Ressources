1fcacadb9ae529a83b2f3a93abd601ef28f4de9a
patriceandrea <andrea_pavico98@yahoo.ca>
2022-08-30 14:17:25 -0400

    adds array function

--- php/array_functions.php
+++ php/array_functions.php
@@ -55,3 +55,85 @@ $merged_values = array_values($merged);
 
 //outputs the values only 
 print_r($merged_values);
+
+
+// array_keys($arr)
+//return the keys from an array 
+
+$keys = array_keys($merged);
+
+print_r($keys);
+
+
+//array_pop($arr)
+//removes last element of array from the end of the array
+
+array_pop($lamborghinis);
+print_r($lamborghinis);
+
+
+// array_push($arr)
+// add a new element at the end of the array 
+array_push($lamborghinis, "Aventador");
+array_push($lamborghinis, "Estoque");
+
+print_r($lamborghinis);
+
+
+//array_shift($arr)
+//used to remove the first element of the array 
+
+array_shift($lamborghinis);
+print_r($lamborghinis);
+
+//sort($arr)
+// sorts array elements in ascending order
+// sorted in alphabetical order 
+
+//sort an array
+sort($lamborghinis);
+
+print_r($lamborghinis);
+
+
+//array_map(function_name, $arr)
+//similar to for loop and foreach 
+
+function addOne($val)
+{
+  return ($val + 1);
+}
+
+$numbers = array(10, 20, 30, 40, 50);
+
+$numbers = array_map('addOne', $numbers);
+
+print_r($numbers);
+
+
+//array_flip($arr)
+//interchange the kets and values of an associative array 
+
+print_r(array_flip($hatchbacks));
+
+
+//array_reverse($arr)
+//reverse the order of elements
+
+print_r(array_reverse($numbers));
+
+
+//array_rand($arr)
+//chose a random element of the array 
+
+$colors = array("red", "black", "blue", "green", "white", "yellow");
+
+echo "Color of the day: " . $colors[array_rand($colors)];
+
+
+//array_slice($arr, $offset, $length)
+//creates a subset of any array 
+//$offset = start 
+//$length = number of elements required in subset 
+
+print_r(array_slice($colors, 2, 3));
